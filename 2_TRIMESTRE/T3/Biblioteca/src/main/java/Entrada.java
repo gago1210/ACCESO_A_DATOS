e0210580d618ecff887ed031a8ada71a1da8c3c3
gago1210 <151818960+gago1210@users.noreply.github.com>
2025-02-13 11:46:28 +0100

    23/01

---
+++ 2_TRIMESTRE/T3/Biblioteca/src/main/java/Entrada.java
@@ -0,0 +1,14 @@
+import Controller.BibliotecaController;
+import controller.BibliotecaController;
+import model.Autor;
+
+public class Entrada {
+    public static void main(String[] args) {
+        BibliotecaController bibliotecaController = new BibliotecaController();
+        bibliotecaController.agregarAutor(new Autor ("Neco", "Ruiz", 2003-10-07));
+    }
+
+
+
+
+}
